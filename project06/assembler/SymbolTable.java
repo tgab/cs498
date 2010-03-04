9359735c6f90817fc48a11ce3defa1e83a7b4ec3
tgab <thansen@mines.edu>
2010-03-03 21:54:31 -0700

    Outline of API created for SymbolTable.java

---
+++ project06/assembler/SymbolTable.java
@@ -0,0 +1,31 @@
+// SymbolTable.java
+// Keeps a correspondence between symbolic labels and
+// numeric addresses
+
+import java.util.Hashtable;
+
+public class SymbolTable {
+
+  private Hashtable table;
+
+  // constructor
+  public SymbolTable() {
+
+  }
+
+  // adds the pair (symbol, address) to the table
+  public void addEntry(String symbol, Integer address) {
+
+  }
+
+  // checks if the symbol table contains a given symbol
+  public Boolean contains(String symbol) {
+    return false;
+  }
+
+  // returns the address associated with a given symbol
+  public int getAddress(String symbol) {
+    return 0;
+  }
+
+}
