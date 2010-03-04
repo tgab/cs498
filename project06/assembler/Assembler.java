d9af1d2c402fd06073ebd5de28a53f79555d26c2
tgab <thansen@mines.edu>
2010-03-04 16:07:30 -0700

    Added code to Assembler to improve file I/O

--- project06/assembler/Assembler.java
+++ project06/assembler/Assembler.java
@@ -4,18 +4,30 @@
 // Usage: java Assembler.java <filename.asm> <filename.hack>
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Assembler {
 
   public static void main(String[] args) throws IOException{
     // Check input arguments
-    if (args.length != 2) {
-      System.err.println("Usage: java Assembler <file.asm> <file.hack>");
+    // Check that file is proper file type
+    if (args.length != 1 || args[0].endsWith(".asm") != true) {
+      System.err.println("Usage: java Assembler <Prog.asm>");
       System.exit(1);
     }
 
+    String inputFile = args[0];
+
     // Create file stream for input
-    BufferedReader asm = new BufferedReader(new FileReader(args[0]));
+    BufferedReader asm = null;
+    try {
+      asm = new BufferedReader(new FileReader(inputFile));
+
+    }
+    catch (FileNotFoundException e) {
+      System.err.println(inputFile + ": does not exist or cannot be read");
+      System.exit(1);
+    }
 
     // Get first line of file
     String line = asm.readLine();
@@ -30,8 +42,17 @@ public class Assembler {
     // Close file stream
     asm.close();
 
+    // Parse out filename for hack file
+    StringTokenizer inputTokens = new StringTokenizer(inputFile, "/");
+    String fileName = null;
+    while (inputTokens.hasMoreElements()) {
+      fileName = inputTokens.nextToken();
+    }
+    fileName = fileName.substring(0, fileName.length()-4) + ".hack";
+    System.out.println(fileName);
+
     // Create file stream for output (***add testing for file existence, writeability, etc)
-    Writer hack = new BufferedWriter(new FileWriter(args[1]));
+    Writer hack = new BufferedWriter(new FileWriter(fileName));
 
     // Write test output to hack
     hack.write("test");
