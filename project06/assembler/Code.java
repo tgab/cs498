0265024d3dca27cd6d2ac37d1207a23d60ee44a4
tgab <thansen@mines.edu>
2010-03-03 21:43:56 -0700

    Outline of API created for Code.java

---
+++ project06/assembler/Code.java
@@ -0,0 +1,26 @@
+// Code.java
+// Translates Hack assembly language mnemonics into binary code
+
+public class Code {
+
+  // constructor
+  public Code(){
+
+  }
+
+  // returns binary code of the dest mnemonic
+  public int dest(String d) {
+    return 0;
+  }
+
+  // returns binary code of the comp mnemonic
+  public int comp(String c) {
+    return 0;
+  }
+
+  // returns binary code of the jump mnemonic
+  public int jump(String j) {
+    return 0;
+  }
+
+}
