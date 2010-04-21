2f17c5dfff15f8bdeb9d1649eaacc8e04aa4aa87
elduncan <elduncan@mines.edu>
2010-04-21 16:06:46 -0600

    Pushing class-parsing stuff - emailed you about a couple question/issues

--- project10/SyntaxAnalyzer/CompilationEngine.java
+++ project10/SyntaxAnalyzer/CompilationEngine.java
@@ -18,69 +18,39 @@ public class CompilationEngine {
 
   // Compiles a complete class
   public void CompileClass() throws IOException {
-	//print default <tokens>
+	
+	 System.out.println("Tokenizer is: " + tokenizer);
+	 if(tokenizer.equals("class")) {										//this never executes - the comparison doesn't work properly											
+	 	outStream.write("<lala> " + tokenizer + " </keyword>\n");
+		System.out.println("<keyword> " + tokenizer + " </keyword>");
+		tokenizer.advance();
+		outStream.write("<identifier> " + tokenizer + " </identifier>\n");
+		tokenizer.advance();
+		outStream.write("<symbol> " + tokenizer + " </symbol>\n");
+		tokenizer.advance();
+		if(tokenizer.equals("static") || tokenizer.equals("field")) {
+			CompileClassVarDec();
+		}else if(tokenizer.equals("constructor") || tokenizer.equals("function") || tokenizer.equals("method")) {
+			CompileSubroutine();
+		}
+		
+	 }
+	//print default <tokens>									//IDK why this is here....
 	outStream.write("<tokens>\n");
 	// Loop through tokens and handle each one
     while (tokenizer.hasMoreTokens()) {
 	  //assigns token's type
       token_type = tokenizer.tokenType();
-	//returns token's corresponding XML line
-		if(token_type == Token.KEYWORD) {
-			outStream.write("<keyword> " + tokenizer.keyWord() + " </keyword>\n");
-		}
-		if(token_type == Token.SYMBOL) {
-			String x = Character.toString(tokenizer.symbol());
-			if(x.equals("<")) {
-				x = "&lt;";
-			}
-			if(x.equals(">")) {
-				x = "&gt;";
-			}
-			if(x.equals("&")) {
-				x = "&amp;";
-			}
-			outStream.write("<symbol> " + x + " </symbol>\n");
-		}
-		if(token_type == Token.IDENTIFIER) {
-			outStream.write("<identifier> " + tokenizer.identifier() + " </identifier>\n");
-		}
-		if(token_type == Token.INT_CONST) {	
-			outStream.write("<integerConstant> " + tokenizer.intVal() + " </integerConstant>\n");
-		}
-		if(token_type == Token.STRING_CONST) {
-			outStream.write("<stringConstant> " + tokenizer.stringVal() + " </stringConstant>\n");
-		}
+
+    	//returns token's corresponding XML line
+		OutputXML(token_type);
 		
       tokenizer.advance();
     }
 
     // Handle last token
     token_type = tokenizer.tokenType();
-	if(token_type == Token.KEYWORD) {
-		outStream.write("<keyword> " + tokenizer.keyWord() + " </keyword>\n");
-	}
-	if(token_type == Token.SYMBOL) {
-		String x = Character.toString(tokenizer.symbol());
-		if(x.equals("<")) {
-			x = "&lt;";
-		}
-		if(x.equals(">")) {
-			x = "&gt;";
-		}
-		if(x.equals("&")) {
-			x = "&amp;";
-		}
-		outStream.write("<symbol> " + x + " </symbol>\n");
-	}
-	if(token_type == Token.IDENTIFIER) {
-		outStream.write("<identifier> " + tokenizer.identifier() + " </identifier>\n");
-	}
-	if(token_type == Token.INT_CONST) {	
-		outStream.write("<integerConstant> " + tokenizer.intVal() + " </integerConstant>\n");
-	}
-	if(token_type == Token.STRING_CONST) {
-		outStream.write("<stringConstant> " + tokenizer.stringVal() + " </stringConstant>\n");
-	}
+	OutputXML(token_type);
 	
 	outStream.write("</tokens>\n");
   
@@ -93,17 +63,56 @@ public class CompilationEngine {
   
   // Compiles a complete method, function, or constructor
   public void CompileSubroutine() {
-  
+	outStream.write("<subroutineDec>\n");
+	outStream.write("<keyword> " + tokenizer + " </keyword>\n");
+	tokenizer.advance();
+	OutputXML(token_type);
+	tokenizer.advance();
+	outStream.write("<identifier> " + tokenizer + " </identifier>\n");
+	tokenizer.advance();
+  	outStream.write("<symbol> " + tokenizer + " </symbol>\n");
+	tokenizer.advance();
+	//Parameter list
+	compileParameterList();
+	outStream.write("<symbol> " + tokenizer + " </symbol>\n");
+	//Subroutine body:
+	outStream.write("<subroutineBody>\n");
+	tokenizer.advance();
+	outStream.write("<symbol> " + tokenizer + " </symbol>\n");
+	tokenizer.advance();
+	compileVarDec();
   }
   
   // Compiles a parameter list
   public void compileParameterList() {
-  
+	outStream.write("<parameterList>\n");
+	while (tokenizer.hasMoreTokens()) {
+	  //assigns token's type
+      token_type = tokenizer.tokenType();
+
+    	//returns token's corresponding XML line
+		OutputXML(token_type);
+		
+      tokenizer.advance();
+    }
+	// Handle last token
+    token_type = tokenizer.tokenType();
+	OutputXML(token_type);
+	
+	outStream.write("</parameterList>\n");
+	//Advance to the ')' symbol
+	tokenizer.advance();
+	System.out.println(tokenizer + " should be ')', otherwise check up on compileParameterList");
   }
   
   // Compiles a var declaration
   public void compileVarDec() {
-  
+  	outStream.write("<varDec>\n");
+	outStream.write("<keyword> " + tokenizer + " </keyword>\n");
+	tokenizer.advance();
+	outStream.write("<keyword> " + tokenizer + " </keyword>\n");
+	tokenizer.advance();
+	outStream.write("<identifier> " + tokenizer + " </identifier>\n");
   }
   
   // Compiles a sentence of statements
@@ -150,5 +159,51 @@ public class CompilationEngine {
   public void CompileExpressionList() {
   
   }
-
+  public void OutputXML(Token token_type) {
+		if(token_type == Token.KEYWORD) {
+			try{
+				outStream.write("<keyword> " + tokenizer.keyWord() + " </keyword>\n");
+			}catch(IOException x){
+				//TODO: print error?
+			}
+		}
+		if(token_type == Token.SYMBOL) {
+			String x = Character.toString(tokenizer.symbol());
+			if(x.equals("<")) {
+				x = "&lt;";
+			}
+			if(x.equals(">")) {
+				x = "&gt;";
+			}
+			if(x.equals("&")) {
+				x = "&amp;";
+			}
+			try{
+				outStream.write("<symbol> " + x + " </symbol>\n");
+			}catch(IOException z){
+				System.err.println("error outputing XML");
+			}
+		}
+		if(token_type == Token.IDENTIFIER) {
+			try{
+				outStream.write("<identifier> " + tokenizer.identifier() + " </identifier>\n");
+			}catch(IOException h){
+				System.err.println("error outputing XML");
+			}
+		}
+		if(token_type == Token.INT_CONST) {	
+			try{
+				outStream.write("<integerConstant> " + tokenizer.intVal() + " </integerConstant>\n");
+			}catch(IOException k) {
+				System.err.println("error outputing XML");
+			}
+		}
+		if(token_type == Token.STRING_CONST) {
+			try{
+				outStream.write("<stringConstant> " + tokenizer.stringVal() + " </stringConstant>\n");
+			}catch(IOException m){
+				System.err.println("error outputing XML");
+			}
+		}
+  }
 }
