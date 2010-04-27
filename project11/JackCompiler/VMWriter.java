5e88337d9ee03911ab5f0c5d5d94f05b19c64e59
tgab <thansen@mines.edu>
2010-04-26 20:00:46 -0600

    Stubbing out VMWriter.

---
+++ project11/JackCompiler/VMWriter.java
@@ -0,0 +1,68 @@
+// VMWriter.java
+
+package JackCompiler;
+
+public class VMWriter {
+	public enum Segment {
+		CONST, ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP
+	}
+	
+	public enum Command {
+		ADD, SUB, NEG, EQ, GT, LT, AND, OR, NOT
+	}
+
+	// Creates a new file and prepares it for writing
+	public VMWriter () {
+	
+	}
+	
+	// Writes a VM push command
+	public void writePush (Segment s, int index) {
+	
+	}
+	
+	// Writes a VM pop command
+	public void writePop (Segment s, int index) {
+	
+	}
+	
+	// Writes a VM arithmetic command
+	public void writeArithmetic (Command c) {
+	
+	}
+	
+	// Writes a VM label command
+	public void writeLabel (String label) {
+	
+	}
+	
+	// Writes a VM goto command
+	public void writeGoto (String label) {
+	
+	}
+	
+	// Writes a VM if-goto command
+	public void writeIf (String label) {
+	
+	}
+	
+	// Writes a VM call command
+	public void writeCall (String name, int nArgs) {
+	
+	}
+	
+	// Writes a VM function command
+	public void writeFunction (String name, int nLocals) {
+	
+	}
+	
+	// Writes a VM return command
+	public void writeReturn () {
+	
+	}
+	
+	// Closes the output file
+	public void close() {
+	
+	}
+}
